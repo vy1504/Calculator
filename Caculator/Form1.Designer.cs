faff95e280d59ef82b69ab665238efde4e9e7a2d
hoangvy <hoangvy.nthv0@gmail.com>
2021-10-30 22:00:32 +0700

    Updata Form1

--- Caculator/Form1.Designer.cs
+++ Caculator/Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txttinhtoan = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.Label();
+            this.txtketqua = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(387, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txttinhtoan
+            // 
+            this.txttinhtoan.AutoSize = true;
+            this.txttinhtoan.Font = new System.Drawing.Font("Times New Roman", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttinhtoan.Location = new System.Drawing.Point(190, 20);
+            this.txttinhtoan.Name = "txttinhtoan";
+            this.txttinhtoan.Size = new System.Drawing.Size(179, 34);
+            this.txttinhtoan.TabIndex = 1;
+            this.txttinhtoan.Text = "TÍNH TOÁN";
+            this.txttinhtoan.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(220, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtA
+            // 
+            this.txtA.AutoSize = true;
+            this.txtA.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtA.Location = new System.Drawing.Point(76, 87);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(59, 27);
+            this.txtA.TabIndex = 1;
+            this.txtA.Text = "Số a:";
+            this.txtA.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(220, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // txtB
+            // 
+            this.txtB.AutoSize = true;
+            this.txtB.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtB.Location = new System.Drawing.Point(75, 145);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(60, 27);
+            this.txtB.TabIndex = 1;
+            this.txtB.Text = "Số b:";
+            this.txtB.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtketqua
+            // 
+            this.txtketqua.AutoSize = true;
+            this.txtketqua.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtketqua.Location = new System.Drawing.Point(75, 204);
+            this.txtketqua.Name = "txtketqua";
+            this.txtketqua.Size = new System.Drawing.Size(94, 27);
+            this.txtketqua.TabIndex = 1;
+            this.txtketqua.Text = "Kết quả:";
+            this.txtketqua.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(220, 204);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(560, 315);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtketqua);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txttinhtoan);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tính toán ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label txttinhtoan;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtA;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label txtB;
+        private System.Windows.Forms.Label txtketqua;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Caculator/Form1.cs
+++ Caculator/Form1.cs
@@ -15,5 +15,15 @@ namespace Caculator
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
